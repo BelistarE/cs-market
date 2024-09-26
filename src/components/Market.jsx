4d3651662ac8d04a0cb5fb184318c7478e765bd3
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-09-26 15:56:48 -0700

    added prices json

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import styles from "./css_modules/market.module.css"; // CSS module for styling
+import styles from "./css_modules/market.module.css";
+import ArrowIcon from "../assets/arrow.svg";
 
 function Market() {
   const { categoryName } = useParams(); // Get the category name from the URL
@@ -49,6 +50,35 @@ function Market() {
 
   return (
     <div>
+      <div className={styles.uppermarket}>
+        <div className={styles.left}>
+          <p>{categoryName}</p>
+          <svg
+            fill="#000000"
+            height="800px"
+            width="800px"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 330 330"
+            xmlSpace="preserve"
+            className={styles.arrow}
+          >
+            <path
+              id="XMLID_222_"
+              d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+	C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+	C255,161.018,253.42,157.202,250.606,154.389z"
+            />
+          </svg>
+          <p className={styles.subcat}> all {categoryName}</p>
+        </div>
+
+        <p>{filteredItems.length} items</p>
+      </div>
+
       <div className={styles.grid}>
         {filteredItems.map((item) => {
           const itemName = item.name.split("|").pop().trim(); // Remove '|' and everything before it
