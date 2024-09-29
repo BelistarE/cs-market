6e03c332723b7d3b5acca59a6a8885c6f467f7bf
BelistarE <beli.estefano@gmail.com>
2024-09-29 00:55:14 -0700

    home styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import one from "../assets/slideshow/1carousel.jpg";
 import two from "../assets/slideshow/2carousel.webp";
-import three from "../assets/slideshow/3carousel.webp";
+import seven from "../assets/slideshow/7carousel.webp";
 import styles from "./css_modules/home.module.css"; // Assuming this is where your CSS is
 
-const images = [one, two, three];
+const images = [one, two, seven];
 
 const PrevArrow = ({ onClick }) => (
   <div
@@ -37,27 +37,52 @@ const Home = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 100000,
+    autoplaySpeed: 3000,
     pauseOnHover: false,
-    arrows: false, // Disable default arrows
+    arrows: false,
   };
 
   return (
-    <div className={styles.carouselContainer}>
-      <Slider ref={slider} {...settings}>
-        {images.map((image, index) => (
-          <div key={index}>
-            <img
-              src={image}
-              alt={`Slide ${index}`}
-              className={styles.carouselImage}
-            />
+    <div className={styles.home}>
+      <div className={styles.carouselContainer}>
+        <Slider ref={slider} {...settings}>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img
+                src={image}
+                alt={`Slide ${index}`}
+                className={styles.carouselImage}
+              />
+            </div>
+          ))}
+        </Slider>
+        <PrevArrow onClick={() => slider.current.slickPrev()} />
+        <NextArrow onClick={() => slider.current.slickNext()} />
+      </div>
+      <footer className={styles.footer}>
+        <div className={styles.footerLinks}>
+          <div className={styles.footerleft}>
+            <a>About us</a>
+            <a>Affiliate</a>
+            <a>Blog</a>
+            <a>Screenshot Tool</a>
+            <a href="https://github.com/ByMykel/CSGO-API">API</a>
           </div>
-        ))}
-      </Slider>
-      {/* Manually render custom arrows */}
-      <PrevArrow onClick={() => slider.current.slickPrev()} />
-      <NextArrow onClick={() => slider.current.slickNext()} />
+          <div className={styles.footerright}>
+            <a>FAQ</a>
+            <a>Support</a>
+            <a>Jobs</a>
+            <a>Privacy</a>
+            <a>Imprint</a>
+            <a>Terms of Service</a>
+          </div>
+        </div>
+        <div className={styles.footercontact}>
+          <p>info@nyaport.com</p>
+          <p>© 2024 Isabeli Estefano</p>
+          <p>Powered by Steam. Not affiliated with Valve Corp.</p>
+        </div>
+      </footer>
     </div>
   );
 };
